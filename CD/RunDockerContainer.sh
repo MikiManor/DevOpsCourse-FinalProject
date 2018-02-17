d2c97fa7148a9dfc5f596c0104efee052e5383d9
Miki MAnor <mikimanor@gmail.com>
2018-02-17 09:16:54 +0000

    added name tag to container

--- CD/RunDockerContainer.sh
+++ CD/RunDockerContainer.sh
@@ -1,1 +1,12 @@
-docker run --name=tomcat -p 80:8080 -dt mikimanor/tomcat:alpine-modified
+containerName="tomcat"
+if [ ! "$(docker ps -q -f name=${containerName})" ]; then
+    docker stop ${containerName}
+    if [ "$(docker ps -aq -f status=exited -f name=${containerName})" ]; then
+        # cleanup
+        docker rm ${containerName}
+    fi
+    # run your container
+    docker run -d --name ${containerName} -p 80:8080 -dt mikimanor/tomcat:alpine-modified
+fi
+
+
