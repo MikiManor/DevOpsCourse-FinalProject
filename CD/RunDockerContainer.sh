622265d3bb7f20e7fa2549f7d6a21b31cbdcd369
Miki Manor <mikimanor@gmail.com>
2018-02-22 09:56:13 +0200

    Update RunDockerContainer.sh

--- CD/RunDockerContainer.sh
+++ CD/RunDockerContainer.sh
@@ -1,4 +1,6 @@
-containerName="tomcat"
+
+containerName=$1
+containerTag=$2
 if [  "$(docker ps -q -f name=${containerName})" ]; then
     docker stop ${containerName}
     if [ "$(docker ps -aq -f status=exited -f name=${containerName})" ]; then
@@ -6,7 +8,7 @@ if [  "$(docker ps -q -f name=${containerName})" ]; then
         docker rm ${containerName}
     fi
     # run your container
-    docker run -d --name ${containerName} -p 80:8080 -dt mikimanor/tomcat:alpine-modified
+    docker run -d --name ${containerName} -p 80:8080 -dt ${containerName}:${containerTag}
 fi
 
 
