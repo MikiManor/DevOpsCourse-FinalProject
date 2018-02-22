b2d15bcf7b2b7b2edca5531d81c4c25490312993
Miki Manor <mikimanor@gmail.com>
2018-02-22 14:10:24 +0200

    Update RunDockerContainer.sh

--- CD/RunDockerContainer.sh
+++ CD/RunDockerContainer.sh
@@ -6,10 +6,10 @@ pureName=`cut -d "/" -f2 <<< ${containerName}`
 
 if [  "$(docker ps -q -f name=${pureName})" ]; then
     docker stop ${pureName}
-    if [ "$(docker ps -aq -f status=exited -f name=${pureName})" ]; then
-        # cleanup
-        docker rm ${pureName}
-    fi
+fi
+if [ "$(docker ps -aq -f status=exited -f name=${pureName})" ]; then
+    # cleanup
+    docker rm ${pureName}
 fi
 # run your container
 docker run -d --name ${pureName} -p 80:8080 -dt ${containerName}:${containerTag}
